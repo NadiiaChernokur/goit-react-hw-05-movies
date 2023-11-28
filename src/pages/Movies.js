24f57a289fa6f42269037003a4768c0563367988
NadiiaChernokur <chiernokur5@gmail.com>
2023-11-28 16:35:56 +0100

    logi

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -11,7 +11,6 @@ const Movies = () => {
   const [error, setError] = useState(null);
   const [params, setParams] = useSearchParams();
   const filter = params.get('query') ?? '';
-  console.log('0000000000');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -62,63 +61,4 @@ const Movies = () => {
   );
 };
 
-// const Movies = () => {
-//   const [allMovies, setAllMovies] = useState([]);
-//   const [status, setStatus] = useState(IDEL);
-//   const [result, setResalt] = useState();
-
-//   const [params, setParams] = useSearchParams();
-//   const filter = params.get('query') ?? '';
-
-//   useEffect(() => {
-//     if (status === IDEL) {
-//       return;
-//     }
-
-//     const fetchData = async () => {
-//       try {
-//         const response = await getFilterMovies(result);
-//         // const rej = response.data.total_results;
-
-//         // if (rej === 0) {
-//         //   setStatus(REJECTED);
-//         //   return;
-//         // }
-//         const trendFilm = response.data.results;
-//         setAllMovies(trendFilm);
-//       } catch (error) {
-//         return;
-//       }
-//     };
-
-//     fetchData();
-//     setStatus(IDEL);
-//   }, [allMovies, filter, result, status]);
-
-//   //   const toFilterArray = () => {
-//   //     // setAllMovies([]);
-//   //     // setStatus(CHENGE);
-//   //   };
-
-//   const visibleArray = e => {
-//     e.preventDefault();
-//     const filterValue = e.target[0].value;
-//     setParams({ query: filterValue });
-//     setResalt(filterValue);
-//     setStatus(PENDING);
-//   };
-
-//   return (
-//     <div>
-//       <Form onSubmit={v => visibleArray(v)}>
-//         <input></input>
-
-//         <button type="submit">Search</button>
-//       </Form>
-//       {status === PENDING && <div> Завантажуємо</div>}
-//       {status === REJECTED && <div> По вашому запиту немає фільмів</div>}
-//       <List>{allMovies.length > 0 && <VisibleArray array={allMovies} />}</List>
-//     </div>
-//   );
-// };
 export default Movies;
